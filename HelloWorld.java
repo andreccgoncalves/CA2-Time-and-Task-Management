5b2957db7670c2beb4613677c694c0686112ad3c
Gantogtokh Battumur <b.ganaa592@gmail.com>
2020-11-23 21:25:44 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -42,9 +42,29 @@ public static void main(String[] args) {
 
 System.out.println("Hello World! - Andre Goncalves, Brazil ");
 System.out.println("Hello World! Lucas Gustavo, Brazil ");
-    
+ System.out.println("Hello World! Gantogtokh Battumur, Mongolia ");   
 calculator_sum();    
 caclulator_multiplication();
 
+}
+     public class Method {
+ static void GantogtokhMethod(){
+    System.out.println("My name is Gantogtokh Battumur, this is my code");
+ }
+    public static void main(String[] args) {
+      System.out.println("This is main program");
+    
+      GantogtokhMethod();
+      System.out.println("Please enter a number");
+
+
+Scanner myKb = new Scanner(System.in);
+int num3 = myKb.nextInt();
+
+System.out.println("Please enter another number");
+int num4 = myKb.nextInt();
+
+System.out.println("The divide  of " + num3 + " and " + num4 + " is " + (num3 / num4));
+   
 }
 }
