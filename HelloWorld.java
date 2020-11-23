07ea4507faa62f2d7f503490d5a87092ddb9c06c
andreccgoncalves <72031101+andreccgoncalves@users.noreply.github.com>
2020-11-23 16:47:58 +0000

    Added two calculator (sum and multiplication)

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,13 @@
 package CA2TimeandTask;
 
+import java.util.Scanner;
+
+
 public class Ca2_timeAndTask {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello World! - Andre Goncalves, Brazil ");
-        System.out.println("Hello World! Lucas Gustavo, Brazil ");
-    }
-     //Enter your command line bellow, so we can test out the shared file and edition, please! :)
     
     
-    
-    
-private static class calculator {
-   
-    
-public calculator() {
-
-            
-System.out.println("Please enter a number");
+static void calculator_sum(){
+  System.out.println("Please enter a number");
 
 
 Scanner myKb = new Scanner(System.in);
@@ -30,10 +19,33 @@ int num2 = myKb.nextInt();
 System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
 
         }
-    }
+
+
+    static void caclulator_multiplication(){
+        System.out.println("Please enter a number");
+
+
+Scanner myKb = new Scanner(System.in);
+int num1 = myKb.nextInt();
+
+System.out.println("Please enter another number");
+int num2 = myKb.nextInt();
+
+System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+
+        }
+    
+    
+    
+public static void main(String[] args) {
+    
+
+System.out.println("Hello World! - Andre Goncalves, Brazil ");
+System.out.println("Hello World! Lucas Gustavo, Brazil ");
+    
+calculator_sum();    
+caclulator_multiplication();
+
+
 }
-
-
-
-
-
+}
