5e4240440594082abc1c4b31ed9a35656770eda9
andreccgoncalves <72031101+andreccgoncalves@users.noreply.github.com>
2020-11-23 15:21:49 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,15 @@
-package helloworld;
+package CA2TimeandTask;
 
-public class HelloWorld {
+public class Ca2_timeAndTask {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World! - Andre Goncalves, Brazil", "Hello World! Lucas Gustavo, Brazil");
+        System.out.println("Hello World! - Andre Goncalves, Brazil ");
+        System.out.println("Hello World! Lucas Gustavo, Brazil ");
     }
      //Enter your command line bellow, so we can test out the shared file and edition, please! :)
 }
+
+
+
+
