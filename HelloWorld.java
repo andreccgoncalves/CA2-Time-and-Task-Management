05e227fb273b37352bede87148c6ee40e6d6a506
andreccgoncalves <72031101+andreccgoncalves@users.noreply.github.com>
2020-11-24 12:32:17 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 public class Ca2_timeAndTask {
     
-//Method done by Andre Goncalves    
+    
 static void calculator_sum(){
-  System.out.println("Please enter a number");
+System.out.println("Please enter a number");
 
 
 Scanner myKb = new Scanner(System.in);
@@ -18,10 +18,11 @@ int num2 = myKb.nextInt();
 
 System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
 
-        }
+}
 
-//Method done by Andre Goncalves
+
 static void caclulator_multiplication(){
+    
 System.out.println("Please enter a number");
 
 
@@ -35,30 +36,29 @@ System.out.println("The multiplication of " + num1 + " and " + num2 + " is " + (
 
 }
     
-    
+static void GantogtokhMethod(){
+    System.out.println("My name is Gantogtokh Battumur, this is my code");
+ }   
     
 public static void main(String[] args) {
     
 
 System.out.println("Hello World! - Andre Goncalves, Brazil ");
 System.out.println("Hello World! Lucas Gustavo, Brazil ");
- System.out.println("Hello World! Gantogtokh Battumur, Mongolia ");   
-calculator_sum();    
-caclulator_multiplication();
+System.out.println("Hello World! Gantogtokh Battumur, Mongolia "); 
 
-}
-     public class Method {
- static void GantogtokhMethod(){
-    System.out.println("My name is Gantogtokh Battumur, this is my code");
- }
-    public static void main(String[] args) {
-      System.out.println("This is main program");
-          GantogtokhMethod();
-      System.out.println("Please enter a number");
+System.out.println("This is main program");
+GantogtokhMethod();
+System.out.println("Please enter a number");
 Scanner myKb = new Scanner(System.in);
 int num3 = myKb.nextInt();
 System.out.println("Please enter another number");
 int num4 = myKb.nextInt();
 System.out.println("The divide  of " + num3 + " and " + num4 + " is " + (num3 / num4));
-   }
+   
+calculator_sum();    
+caclulator_multiplication();
+
+
+}
 }
