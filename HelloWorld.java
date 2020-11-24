f8e84954be56f9b5e0605a3f6cee6ccdca8fbe21
andreccgoncalves <72031101+andreccgoncalves@users.noreply.github.com>
2020-11-24 13:25:43 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,8 +8,6 @@ public class Ca2_timeAndTask {
 //Method done by Andre Goncalves    
 static void calculator_sum(){
 System.out.println("Please enter a number");
-
-
 Scanner myKb = new Scanner(System.in);
 int num1 = myKb.nextInt();
 
@@ -17,14 +15,12 @@ System.out.println("Please enter another number");
 int num2 = myKb.nextInt();
 
 System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
-
 }
 
 //Method done by Andre Goncalves
 static void caclulator_multiplication(){
     
 System.out.println("Please enter a number");
-
 
 Scanner myKb = new Scanner(System.in);
 int num1 = myKb.nextInt();
@@ -38,7 +34,6 @@ System.out.println("The multiplication of " + num1 + " and " + num2 + " is " + (
  
 public static void main(String[] args) {
     
-
 System.out.println("Hello World! - Andre Goncalves, Brazil ");
 System.out.println("Hello World! Lucas Gustavo, Brazil ");
 System.out.println("Hello World! Gantogtokh Battumur, Mongolia "); 
