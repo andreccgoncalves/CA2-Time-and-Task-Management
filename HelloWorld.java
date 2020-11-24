aec171815e77549a057849c3ff68ac98cdbce40f
andreccgoncalves <72031101+andreccgoncalves@users.noreply.github.com>
2020-11-24 13:39:18 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ package CA2TimeandTask;
 
 import java.util.Scanner;
 
-
 public class Ca2_timeAndTask {
     
 //Method done by Andre Goncalves    
@@ -31,6 +30,18 @@ int num2 = myKb.nextInt();
 System.out.println("The multiplication of " + num1 + " and " + num2 + " is " + (num1 * num2));
 
 }
+
+    //Method done by Gantogtokh Battumur
+static void caclulator_division(){
+
+System.out.println("Please enter a number");
+Scanner myKb = new Scanner(System.in);
+int num3 = myKb.nextInt();
+System.out.println("Please enter another number");
+int num4 = myKb.nextInt();
+
+System.out.println("The division between " + num3 + " and " + num4 + " is " + (num3 / num4));
+}
  
 public static void main(String[] args) {
     
@@ -39,17 +50,11 @@ System.out.println("Hello World! Lucas Gustavo, Brazil ");
 System.out.println("Hello World! Gantogtokh Battumur, Mongolia "); 
 System.out.println("Hello World! Frank Willian Brazil, Brazil "); 
     
-//Method done by Gantogtokh Battumur
-System.out.println("Please enter a number");
-Scanner myKb = new Scanner(System.in);
-int num3 = myKb.nextInt();
-System.out.println("Please enter another number");
-int num4 = myKb.nextInt();
-System.out.println("The divide  of " + num3 + " and " + num4 + " is " + (num3 / num4));
+
    
 calculator_sum();    
 caclulator_multiplication();
-
+caclulator_division();
 
 }
 }
