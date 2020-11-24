ee5bc55e627aceae6ee93a55ce577a2e4d15ce3d
frankire <62744883+frankire@users.noreply.github.com>
2020-11-24 13:45:38 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,52 +9,56 @@ static void calculator_sum(){
 System.out.println("Please enter a number");
 Scanner myKb = new Scanner(System.in);
 int num1 = myKb.nextInt();
-
 System.out.println("Please enter another number");
 int num2 = myKb.nextInt();
-
 System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
 }
-
 //Method done by Andre Goncalves
 static void caclulator_multiplication(){
-    
 System.out.println("Please enter a number");
-
 Scanner myKb = new Scanner(System.in);
 int num1 = myKb.nextInt();
-
 System.out.println("Please enter another number");
 int num2 = myKb.nextInt();
-
 System.out.println("The multiplication of " + num1 + " and " + num2 + " is " + (num1 * num2));
-
 }
-
-    //Method done by Gantogtokh Battumur
+//Method done by Gantogtokh Battumur
 static void caclulator_division(){
-
 System.out.println("Please enter a number");
 Scanner myKb = new Scanner(System.in);
 int num3 = myKb.nextInt();
 System.out.println("Please enter another number");
 int num4 = myKb.nextInt();
-
 System.out.println("The division between " + num3 + " and " + num4 + " is " + (num3 / num4));
 }
- 
+//Method done by Frank Willian
+static void caclulator_subtractionFrankWillian(){
+System.out.println("Please enter a number");
+Scanner myKb = new Scanner(System.in);
+int num1 = myKb.nextInt();
+System.out.println("Please enter another number");
+int num2 = myKb.nextInt();
+System.out.println("The subtraction of " + num1 + " and " + num2 + " is " + (num1 - num2));
+}
 public static void main(String[] args) {
-    
 System.out.println("Hello World! - Andre Goncalves, Brazil ");
 System.out.println("Hello World! Lucas Gustavo, Brazil ");
-System.out.println("Hello World! Gantogtokh Battumur, Mongolia "); 
-System.out.println("Hello World! Frank Willian Brazil, Brazil "); 
-    
-
-   
-calculator_sum();    
+System.out.println("Hello World! Gantogtokh Battumur, Mongolia ");
+System.out.println("Hello World! Frank Willian Brazil, Brazil ");
+calculator_sum();
 caclulator_multiplication();
 caclulator_division();
+caclulator_subtractionFrankWillian();
+}
+}
 
-}
-}
+
+
+
+
+
+
+
+
+
+
